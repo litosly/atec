4c8c725affb518bf01c6a2b53597ebe45061875f
Zhaocong Yuan <justin.yuan@mail.utoronto.ca>
2018-06-17 12:04:38 -0700

    incomplete code for dataloader and encoder-decoder model

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,19 +1,83 @@
 import os 
+import re 
+import csv
 import numpy as np 
 
 import torch 
 from torch.utils.data import Dataset, DataLoader
 
 
+class Lang(object):
+	""" creates word-index mappings 
+	"""
+	def __init__(self, name):
+		self.name = name 
+		self.word2index = {}
+		self.word2count = {}
+		self.index2word = {0: "SOS", 1: "EOS"}
+		self.n_words = 2   
+
+	def addSentence(self, sentence):
+		# need to split according to Chinese sentences or specific input format
+		for word in sentence.split():
+			self.addWord(word)
+
+	def addWord(self, word):
+		if word not in self.word2index:
+			self.word2index[word] = self.n_words
+			self.word2count[word] = 1 
+			self.index2word[self.n_words] = word 
+			self.n_words += 1 
+		else:
+			self.word2count[word] += 1 
+
+
+def unicodeToAscii(s):
+	return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+def normalizeString(s):
+	s = unicodeToAscii(s.lower().strip())
+	s = re.sub(r"([.!?])", r" \1", s)
+	s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+	return s 
+
+def readLangs(path):
+	print("Reading data...")
+
+	with open(data_path, 'rb') as f:
+	    reader = csv.reader(f)
+	    data_list = list(reader)
+	# each item (sent1, sent2, label)
+	item_list = [[w for w in item[0].split('\t')] for item in data_list]
+	# TODO, apply filters here to remove unwanted lexiacal info 
+	# word-index mappings 
+	lang = Lang(path)
+	return item_list, lang 
+
+def prepareData(path):
+	items, lang = readLangs(path)
+	for item in items:
+		lang.addSentence(item[0])
+		lang.addSentence(item[1])
+	print("Counted words: ", lang.n_words)
+	return items, lang 
+
+
 class AtecDataset(Dataset):
 	def __init__(self, config, mode="train"):
-		pass 
+		self.config = config 
+		self.build_dic()
 
 	def __len__(self):
-		return None 
+		return len(self.items)
 
 	def __getitem__(self, index):
-		return None 
+		return self.items[index]
+
+	def build_dic(self):
+		items, lang = prepareData(config.data_path)
+		self.items = items 
+		self.lang = lang 
 
 
 def get_dataloader(config, mode="train"):
--- src/model.py
+++ src/model.py
@@ -3,11 +3,12 @@ import numpy as np
 
 import torch 
 import torch.nn as nn 
+import torch.nn.functional as F 
 
 
-class BaseModel(object):
+class BaseModel(nn.Module):
 	def __init__(self):
-		pass 
+		super(BaseModel, self).__init__()
 
 	def forward(self, x):
 		return None
@@ -16,6 +17,52 @@ class BaseModel(object):
 class AtecModel(BaseModel):
 	def __init__(self, config):
 		super(AtecModel, self).__init__() 
+		self.config = config
+		self.encoder = Encoder(config)
+		self.decoder = Decoder(config)
 
 	def forward(self, x):
 		return None 
+
+
+class Encoder(nn.Module):
+	def __init__(self, config):
+		super(Encoder, self).__init__()
+		self.input_size = config.input_size
+		self.hidden_size = config.hidden_size 
+
+		self.embedding = nn.Embedding(self.input_size, self.hidden_size)
+		self.lstm = nn.LSTM(self, hidden_size, self,hidden_size)
+
+	def forward(self, input, hidden):
+		embedded = self.embedding(input).view(1, 1, -1)
+		output = embedded 
+		output, hidden = self.lstm(output, hidden)
+		return output, hidden 
+
+	def initHidden(self):
+		return torch.zeros(1, 1, self.hidden_size)
+
+
+class Decoder(nn.Module):
+	def __init__(self, config):
+		super(Decoder, self).__init__()
+		self.hidden_size = config.hidden_size
+		self.output_size = config.output_size
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+	def forward(self, input):
+		output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.lstm(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
